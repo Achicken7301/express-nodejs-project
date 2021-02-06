d5b723e619b654e4c9b3f4e5d194794728673ac4
khang <banhbeovodung01@gmail.com>
2021-02-07 01:23:54 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const animals = [
+    {id: 1, name: "dog"},
+    {id: 2, name: "cat"},
+    {id: 3, name: "monkey"},
+]
+
+app.get('/', function(req, res){
+    res.send('this is root of the website!!');
+});
+
+app.get('/api/animals', function(req, res){
+    res.send(animals);
+});
+
+app.get('/api/animalshow/:id', (req, res) => {
+    const animal = animals.find(c => c.id === parseInt(res.params.id));
+    if(!animal) res.status(404).send('Object not found.');
+    res.send(animals);
+})
+
+const port = process.env.port || 5500;
+app.listen(port, function() {console.log(`Listening to port ${port}`)});
