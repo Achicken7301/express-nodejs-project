68cab506d08af36288d656d5044547a9110c749b
danhYYR <danh0989@gmail.com>
2021-02-17 21:40:07 +0700

    Van chua tim cach lay req tu html day qua fife js dc

--- Api.js
+++ Api.js
@@ -1,7 +1,21 @@
-var a = document.getElementById(a).nodeValue
-var b = document.getElementById(b).nodeValue
-function sum(a,b) { var result = a+b 
+const API_KEY ='oA81cTs6tqbOVYHIaW1PuYxHlN79kSnn'
+url = 'https://api.giphy.com/v1/gifs/random';
+const userinput = document.getElementById('try')
+var test
+async function getImage(){
+  const API_KEY ='oA81cTs6tqbOVYHIaW1PuYxHlN79kSnn'
+  url = 'https://api.giphy.com/v1/gifs/random';
+  const FULL_url = url +'?api_key='+API_KEY+'&tag='+ test;
 
-return result = document.getElementById("result").innerHTML
-}
-sum(a,b)
+const result=  await fetch(FULL_url) 
+    .then(res => res.json())
+    .catch(err => console.log(err))
+return result.data.image_url
+  }
+  async function run(){
+      const image =await getImage()
+      const img =document.querySelector("#image")
+      console.log(image)
+      document.getElementById("image").src =image
+    }
+run()
--- Tryapi.js
+++ Tryapi.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
-const https = require('https')
+const https = require('https');
 const fs = require('fs');
-var url = require('url')
-
-app.get('/', function(req, res){
+var url = require('url');
+const fetch = require("node-fetch");
+app.get('/',function(req,res){ 
   fs.readFile('Tryapi.html', function(err, data) {
-      res.writeHead(200, {'Content-type': 'text/html'});
-      res.write(data);
-      return res.end();
-  });
-});
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write(data);
+    return res.end();
+})
+})
+app.get('/Api.js',function(req,res){ 
+  fs.readFile('Api.js', function(err, data) {
+    fs.readFile('Tryapi.html', function(err, data) {
+
+  })
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write(data);
+    return res.end();
+})
+})
 const port = 8080
 app.listen(port, function() {console.log(`Listening to port ${port}`)});
