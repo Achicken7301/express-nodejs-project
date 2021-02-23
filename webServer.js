401f7d18c5f68bd45d99f1e029e74c5484a6d0e4
khang <banhbeovodung01@gmail.com>
2021-02-24 01:11:12 +0700

    Truy cập được vào mDNS

--- webServer.js
+++ webServer.js
@@ -3,12 +3,6 @@ const app = express();
 
 const fs = require('fs');
 
-const animals = [
-    {id: 1, name: "dog"},
-    {id: 2, name: "cat"},
-    {id: 3, name: "monkey"},
-]
-
 app.get('/', function(req, res){
     fs.readFile('index.html', function(err, data) {
         res.writeHead(200, {'Content-type': 'text/html'});
