c7ed187e892e4e22cd5c52fb93cbc5bf20ec65c4
danhYYR <danh0989@gmail.com>
2021-03-10 21:33:51 +0700

    code html chua on dau moi tam ket noi vo server duoc thoi

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function red() {
-    document.getElementById('color-change').style.color = 'red';
-}
+const app = require('express')();
+const http = require('http');
+//const io = require('socket.io')(http);
+const server = http.createServer(app)
 
-function blue() {
-    document.getElementById('color-change').style.color = 'blue';
-}
+const WebSocket = require('ws');
+
+socket = new WebSocket.Server({server});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+socket.on('connection', function (ws,req) {
+  
+
+    const ip = req.socket.remoteAddress
+    console.log("New client connected"+ip)
+
+  console.log('started client interval');
+  ws.on('message', function (msg) {
+    console.log("message: "+msg);
+    });
+  ws.on('close', function () {
+    console.log('stopping client interval');
+
+  });
+});
+server.listen(3484, () => {
+  console.log('listening on *:3484');
+});
